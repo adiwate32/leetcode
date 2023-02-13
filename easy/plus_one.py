b4dbe8ce3475b72e668469bac3c2c86a1de462d1
Abhishek Diwate (adiwate) <adiwate@cisco.com>
2023-02-13 08:06:55 +0530

    feb 13 initial commit

--- easy/plus_one.py
+++ easy/plus_one.py
@@ -29,4 +29,12 @@ from typing import List
 
 def plus_one(digits: List[int]) -> List[int]:
 
-    return list(map(int, str(int("".join(list(map(str, digits)))) + 1)))
+    n = len(digits)
+
+    for i in range(n - 1, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] = digits[i] + 1
+            return digits
+    return [1] + digits
---
+++ medium/multiply_strings.py
@@ -0,0 +1,52 @@
+# Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+# Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+
+# Example 1:
+
+# Input: num1 = "2", num2 = "3"
+# Output: "6"
+# Example 2:
+
+# Input: num1 = "123", num2 = "456"
+# Output: "56088"
+
+# https://leetcode.com/problems/multiply-strings/description/?envType=study-plan&id=level-2
+
+
+def multiply(num1: str, num2: str) -> str:
+
+    # If either of the inputs is "0", return "0"
+    if num1 == "0" or num2 == "0":
+        return "0"
+
+    # Reverse the strings to make them easier to work with
+    nums1 = num1[::-1]
+    nums2 = num2[::-1]
+
+    # Create a list with the length of the sum of the lengths of both inputs
+    # filled with 0's to store the intermediate results
+    res = [0] * (len(nums1) + len(nums2))
+
+    # Loop through each digit in the first number
+    for i1 in range(len(nums1)):
+
+        # Loop through each digit in the second number
+        for i2 in range(len(nums2)):
+
+            # Multiply the digits and add them to the intermediate results list
+            digit = int(nums1[i1]) * int(nums2[i2])
+            res[i1 + i2] += digit
+
+            # Take care of carry over
+            res[i1 + i2 + 1] += res[i1 + i2] // 10
+            res[i1 + i2] = res[i1 + i2] % 10
+
+    # Remove any trailing 0's from the intermediate results list
+    while not res[-1]:
+        res.pop()
+
+    # Convert the intermediate results list to strings and reverse it back
+    res_map = list(map(str, res))
+    return "".join(res_map[::-1])
